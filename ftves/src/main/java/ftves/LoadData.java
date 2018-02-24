ac565b36d62d2d916264cb8a3e7f0ff2fc1e6748
Angelo Corna <angelo_corna@it.ibm.com>
2018-02-24 17:04:27 +0100

    code review

--- ftves/src/main/java/ftves/LoadData.java
+++ ftves/src/main/java/ftves/LoadData.java
@@ -2,14 +2,11 @@ package ftves;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.http.HttpEntity;
@@ -19,8 +16,8 @@ import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
-
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class LoadData 
 {
@@ -51,17 +48,18 @@ public class LoadData
 		JSONObject obj = new JSONObject(EntityUtils.toString(lastHitResponse.getEntity()));
 		int total = obj.getJSONObject("hits").getInt("total");
 		String lastDate = null;
-		BigDecimal lastFtvEnergy=null;
-		BigDecimal lastConEnergy=null;
+		Double lastFtvEnergy=null;
+		Double lastConEnergy=null;
 		if (total == 0){
 			lastDate = "1970-01-01 00:00:00";
 		}else{
 			JSONObject jHits = obj.getJSONObject("hits");
 			JSONArray aHits = jHits.getJSONArray("hits");
 			lastDate = aHits.getJSONObject(0).getJSONObject("_source").getString("timestamp");
-			lastFtvEnergy = aHits.getJSONObject(0).getJSONObject("_source").getBigDecimal("ftvEnergy");
-			lastConEnergy = aHits.getJSONObject(0).getJSONObject("_source").getBigDecimal("conEnergy");
+			lastFtvEnergy = aHits.getJSONObject(0).getJSONObject("_source").getDouble("ftvEnergy");
+			lastConEnergy = aHits.getJSONObject(0).getJSONObject("_source").getDouble("conEnergy");
 		}
+		
 		System.out.println("last insert timestamp is "+lastDate);
 		System.out.println("last ftv energy is "+lastFtvEnergy);
 		System.out.println("last con energy is "+lastConEnergy);
@@ -75,35 +73,49 @@ public class LoadData
 	    ResultSet rs = stmt.executeQuery("select * from rs485data where date > '"+lastDate+"' order by date");
 	    while (rs.next()) {
 	    	String timestampI = rs.getString("date").substring(0,19);
-	    	BigDecimal ftvEnergyI = rs.getBigDecimal("ftv_energy");
-	    	BigDecimal conEnergyI = rs.getBigDecimal("con_energy");
-	    	if ( ftvEnergyI != null &&  conEnergyI != null){
-	    		BigDecimal diffFtvEnergy = new BigDecimal("0.0");
-	    		BigDecimal diffConEnergy = new BigDecimal("0.0");
-		    	if (lastFtvEnergy != null){
-			    	diffFtvEnergy = ftvEnergyI.subtract(lastFtvEnergy);
-			    	diffConEnergy = conEnergyI.subtract(lastConEnergy);
-		    	}
-		    	HttpEntity entityI = new NStringEntity(
+	    	String ftvVoltageS = rs.getString("ftv_voltage");
+	    	String ftvCurrentS = rs.getString("ftv_current");
+	    	String ftvPowerS = rs.getString("ftv_power");
+	    	String ftvFrequencyS = rs.getString("ftv_frequency");
+	    	String ftvEnergyS = rs.getString("ftv_energy");
+	    	String conVoltageS = rs.getString("con_voltage");
+	    	String conCurrentS = rs.getString("con_current");
+	    	String conPowerS = rs.getString("con_power");
+	    	String conFrequencyS = rs.getString("con_frequency");
+	    	String conEnergyS = rs.getString("con_energy");
+	    	
+	    	//insert record if no null values are detected
+	    	if ( ftvVoltageS != null &&  ftvCurrentS != null && ftvPowerS != null && ftvFrequencyS != null && ftvEnergyS != null && conVoltageS != null  && conCurrentS != null && conPowerS != null && conFrequencyS != null && conEnergyS != null ){
+		    	Double ftvEnergyI = rs.getDouble("ftv_energy");
+		    	Double conEnergyI = rs.getDouble("con_energy");
+	    		Double diffFtvEnergy = (double) 0;
+	    		Double diffConEnergy = (double) 0;
+	    		if(lastFtvEnergy != null){
+			    	diffFtvEnergy = ftvEnergyI - lastFtvEnergy;
+			    	diffConEnergy = conEnergyI - lastConEnergy;
+	    		}
+			    HttpEntity entityI = new NStringEntity(
 					"{\n" +
-					"    \"conCurrent\" : "+rs.getFloat("con_current")+",\n" +                                      
+					"    \"conCurrent\" : "+rs.getDouble("con_current")+",\n" +                                      
 					"    \"conEnergy\" : "+conEnergyI+",\n" +                                      
-					"    \"conFrequency\" : "+rs.getFloat("con_frequency")+",\n" +                                      
-					"    \"conPower\" : "+rs.getFloat("con_power")+",\n" +                                      
-					"    \"conVoltage\" : "+rs.getFloat("con_Voltage")+",\n" +                                      
+					"    \"conFrequency\" : "+rs.getDouble("con_frequency")+",\n" +                                      
+					"    \"conPower\" : "+rs.getDouble("con_power")+",\n" +                                      
+					"    \"conVoltage\" : "+rs.getDouble("con_Voltage")+",\n" +                                      
 					"    \"diffCONEnergy\" : "+diffConEnergy+",\n" +                                      
-					"    \"ftvCurrent\" : "+rs.getFloat("ftv_current")+",\n" +                                      
+					"    \"ftvCurrent\" : "+rs.getDouble("ftv_current")+",\n" +                                      
 					"    \"ftvEnergy\" : "+ftvEnergyI+",\n" +                                      
-					"    \"ftvFrequency\" : "+rs.getFloat("ftv_frequency")+",\n" +                                      
-					"    \"ftvPower\" : "+rs.getFloat("ftv_power")+",\n" +                                      
-					"    \"ftvVoltage\" : "+rs.getFloat("ftv_Voltage")+",\n" +                                      
+					"    \"ftvFrequency\" : "+rs.getDouble("ftv_frequency")+",\n" +                                      
+					"    \"ftvPower\" : "+rs.getDouble("ftv_power")+",\n" +                                      
+					"    \"ftvVoltage\" : "+rs.getDouble("ftv_Voltage")+",\n" +                                      
 					"    \"diffFTVEnergy\" : "+diffFtvEnergy+",\n" +                                      
 					"    \"timestamp\" : \""+timestampI+"\"\n" +
 					"}", ContentType.APPLICATION_JSON);
-				Response indexResponse = restClient.performRequest("POST","/ftvlogger/rs485data",Collections.<String, String>emptyMap(),entityI);
-				System.out.println(timestampI+" measurement insert to elasticsearch index");
+				restClient.performRequest("POST","/ftvlogger/rs485data",Collections.<String, String>emptyMap(),entityI);
+				System.out.println(timestampI+" measurement insert in elasticsearch index");
+		    	lastFtvEnergy = ftvEnergyI;
+		    	lastConEnergy = conEnergyI;
 	    	}else{
-				System.out.println(timestampI+" measurement NOT insert to elasticsearch index (null values detected)");
+				System.out.println(timestampI+" measurement NOT insert in elasticsearch index (null values detected)");
 	    	}
 	    }
 	    restClient.close();
