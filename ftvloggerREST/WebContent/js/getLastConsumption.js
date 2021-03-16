75b941b78b7ddc31e1ff1ffb88164a4fc56307c9
Angelo Corna <angelo_corna@it.ibm.com>
2021-03-16 21:58:44 +0100

    REST client

--- ftvloggerREST/WebContent/js/getLastConsumption.js
+++ ftvloggerREST/WebContent/js/getLastConsumption.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.ajax({
-//        url: "http://ftvlogger:8080/ftvRESTful/getLastFtvData",  //set me for release
-        url: "http://localhost:8080/ftvRESTful/getLastFtvData",    //set me for devel
+        url: "https://ftvlogger:8443/ftvRESTful/getLastFtvData",  //set me for release
+//        url: "http://localhost:8080/ftvRESTful/getLastFtvData",    //set me for devel
         headers: {'Access-Control-Allow-Origin': '*' }
     }).then(function(data) {
        $('.greeting-id').append(data.ftv_energy);
